259c2c555b7266e29c66bb5b23d1157b7f65d834
j-winston <kenjameswinston@gmail.com>
2023-04-05 12:16:28 -0400

    add publicPath for images to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+      publicPath: 'https://https://j-winston.github.io/Restaurant-Page/'
   },
   devServer: {
     static: {
